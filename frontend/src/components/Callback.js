a1d86473b4561a5639cb39782d8ea76591700793
Curti.s_ <matthewscurtis.dev81@gmail.com>
2019-05-27 03:01:38 +0300

    - :sparkles: Add callback component
    - It calls handleAuthentication method to fetch user info sent by Auth0
    - It redirects users to the homepage after authentication

---
+++ frontend/src/components/Callback.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import auth0Client from "../utils/Auth";
+
+class Callback extends React.Component {
+  async componentDidMount() {
+    await auth0Client.handleAuthentication();
+    this.props.history.replace("/");
+  }
+
+  render() {
+    <p> Loading... </p>;
+  }
+}
+
+export default withRouter(Callback);
